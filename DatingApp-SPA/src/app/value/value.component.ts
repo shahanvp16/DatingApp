5ef41db2ad49e959ec72f2c59c1e8d04de370a72
shahanvp <shahanvp16@gmail.com>
2020-12-23 22:35:13 +0530

    Initial Commit

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.scss']
+})
+export class ValueComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+    }
+}
